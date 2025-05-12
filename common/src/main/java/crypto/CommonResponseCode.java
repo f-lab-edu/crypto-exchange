1b9adeb41ff4c20f33d1b807f137358ec73a7cde
jeonhs2482 <66204827+jeonhs2482@users.noreply.github.com>
2025-05-13 03:09:46 +0900

    orderService 및 orderController 수정사항 반영

--- common/src/main/java/crypto/CommonResponseCode.java
+++ common/src/main/java/crypto/CommonResponseCode.java
@@ -7,16 +7,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum CommonResponseCode {
 
-    //common
-    SUCCESS(200, "요청이 정상적으로 처리되었습니다."),
-    FAIL(400, "요청이 실패하였습니다."),
+    SUCCESS("요청이 정상적으로 처리되었습니다.");
 
-    // order
-    ORDER_NOT_FOUND(404, "존재하지 않는 주문번호입니다."),
-
-    // user
-    USER_NOT_FOUND(404, "존재하지 않는 유저입니다.");
-
-    private final int code;
     private final String message;
 }
