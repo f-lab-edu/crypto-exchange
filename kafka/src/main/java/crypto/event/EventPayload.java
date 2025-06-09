1d44f1f684b3d6fcffaf9d0dc888f27e674e018f
jeonhs2482 <66204827+jeonhs2482@users.noreply.github.com>
2025-06-10 01:11:05 +0900

    kafka 셋팅로직 추가

---
+++ kafka/src/main/java/crypto/event/EventPayload.java
@@ -0,0 +1,4 @@
+package crypto.event;
+
+public interface EventPayload {
+}
---
+++ kafka/src/test/java/crypto/event/EventTest.java
@@ -0,0 +1,43 @@
+package crypto.event;
+
+import crypto.event.payload.LimitOrderCreateEventPayload;
+import org.junit.jupiter.api.Test;
+
+import static crypto.event.EventType.*;
+import static java.math.BigDecimal.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+class EventTest {
+
+    @Test
+    void checkSerializeAndDeserializeEvent() {
+        // given
+        LimitOrderCreateEventPayload payload = LimitOrderCreateEventPayload.builder()
+                .orderId(1L)
+                .coinId(1L)
+                .price(valueOf(1000))
+                .build();
+
+        Event<EventPayload> event = Event.of(
+                1234L,
+                LIMIT_ORDER_CREATE,
+                payload
+        );
+
+        String json = event.toJson();
+
+        // when
+        Event<EventPayload> result = Event.fromJson(json);
+
+        // then
+        assertThat(result.getEventId()).isEqualTo(event.getEventId());
+        assertThat(result.getType()).isEqualTo(event.getType());
+        assertThat(result.getPayload()).isInstanceOf(payload.getClass());
+
+        LimitOrderCreateEventPayload resultPayload = (LimitOrderCreateEventPayload) result.getPayload();
+        assertThat(resultPayload.getOrderId()).isEqualTo(payload.getOrderId());
+        assertThat(resultPayload.getCoinId()).isEqualTo(payload.getCoinId());
+        assertThat(resultPayload.getPrice()).isEqualTo(payload.getPrice());
+    }
+}
